fb04937a5fc7e59d10a58d9365d32ed83761c061
NaveenNaiduSetti <85381717+SettiNaveenNaidu@users.noreply.github.com>
2023-04-21 02:13:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ import numpy as np
 import sklearn
 from sklearn.preprocessing import StandardScaler
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
+model = pickle.load(open('lasso.pkl', 'rb'))
 @app.route('/',methods=['GET'])
 
 def Home():
-    return render_template('new.html')
+    return render_template('lasso.html')
 
 standard_to = StandardScaler()
 @app.route("/predict", methods=['POST'])
@@ -26,9 +26,9 @@ def predict():
         prediction=model.predict([[Feed,Speed,Doc,Woc,Dir,Coolant]])
         output=round(prediction[0],6)
         
-        return render_template('new.html',prediction_text="The value of roughness is {}".format(output))
+        return render_template('lasso.html',prediction_text="The value of roughness is {}".format(output))
     else:
-        return render_template('new.html')
+        return render_template('lasso.html')
 
 if __name__=="__main__":
     app.run(debug=True)
